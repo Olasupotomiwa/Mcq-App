041d12c574c340fcbedd620473dbc3593fe6dda6
Olasupo Tomiwa <97839385+Olasupotomiwa@users.noreply.github.com>
2022-07-08 05:18:23 +0100

    Update General Mathematics.js

--- Quiz doc/General Mathematics.js
+++ Quiz doc/General Mathematics.js
@@ -7,17 +7,6 @@
 
 
 
-//Function to display loader before loading the actual content
-var myVar;
-
-function myloader() {
- myVar = setTimeout(showPage, 2000);
-}
-
-function showPage() {
-  document.querySelector(".lds-ellipsis").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
 
 
 
